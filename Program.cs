fe0509c920081ff76df7d8e5994436a0e94c8f5c
StrangeSeal-ux <116766600+StrangeSeal-ux@users.noreply.github.com>
2023-08-11 21:45:57 -0500

    create juggernaut mode

--- Program.cs
+++ Program.cs
@@ -1,113 +1,125 @@
 ﻿using BattleBitAPI;
 using BattleBitAPI.Common;
 using BattleBitAPI.Server;
-using System.Threading.Channels;
-using System.Xml;
 
 class Program
 {
     static void Main(string[] args)
     {
-        var listener = new ServerListener<MyPlayer, MyGameServer>();
+        // Create and start the server listener
+        var listener = new ServerListener<MyPlayer, CustomServer>();
         listener.Start(29294);
 
-        Thread.Sleep(-1);
+        System.Threading.Thread.Sleep(-1);
     }
 }
+
 class MyPlayer : Player<MyPlayer>
 {
-    public bool IsZombie;
+    public int NumberOfKills;
+    public int Health = 100;
+    public float Damage = 1f;
+    public float MoveSpeed = 1f;
 }
-class MyGameServer : GameServer<MyPlayer>
+
+class CustomServer : GameServer<MyPlayer>
 {
+    public MyPlayer JuggernautPlayer = null;
 
-    public override async Task OnRoundStarted()
+    public override async Task OnTick()
     {
-    }
-    public override async Task OnRoundEnded()
-    {
+        if (this.RoundSettings.State == GameState.WaitingForPlayers)
+            ForceStartGame();
+
+        await Task.Delay(1000);
     }
 
-    public override async Task OnPlayerConnected(MyPlayer player)
+    public async Task OnRoundStarted()
     {
-        bool anyZombiePlayer = false;
-        foreach (var item in AllPlayers)
+        // Randomly select a player to be the juggernaut
+        var playersList = AllPlayers.ToList();
+        var random = new Random();
+        int randomIndex = random.Next(playersList.Count);
+        JuggernautPlayer = playersList[randomIndex];
+
+        // Apply attributes to the juggernaut player
+        SetJuggernautAttributes(JuggernautPlayer);
+
+
+        // Inform players about the juggernaut and start of the round
+        foreach (var player in AllPlayers)
         {
-            if (item.IsZombie)
+            if (player.Team.Equals(JuggernautPlayer))
             {
-                anyZombiePlayer = true;
-                break;
+                player.Message($"You are the Juggernaut!");
+            }
+            else
+            {
+                player.Message($"The Juggernaut is {JuggernautPlayer.Name}");
+            }
+
+            player.Message("Round has started! Get ready to fight!");
+        }
+    }
+
+    // Method to broadcast a message to all players
+    public void BroadcastMessage(string message)
+    {
+        foreach (var player in AllPlayers)
+        {
+            player.Message(message);
+        }
+    }
+
+
+    // Method to set attributes for the juggernaut player
+    public void SetJuggernautAttributes(MyPlayer player)
+    {
+        player.SetHP(player.Health);
+        player.SetFallDamageMultiplier(0.1f);
+        player.SetPrimaryWeapon(new WeaponItem() { Tool = Weapons.P90, MainSight = Attachments.FYouSight }, 20, false);
+        player.SetRunningSpeedMultiplier(player.MoveSpeed);
+    }
+
+
+    public async Task OnAPlayerKilledAnotherPlayer(MyPlayer killer, MyPlayer victim,
+        OnPlayerKillArguments<MyPlayer> args)
+    {
+        if (killer.Team.Equals(JuggernautPlayer))
+        {
+            if (victim.Team.Equals(JuggernautPlayer))
+            {
+                // assign to random player
+                var playersList = AllPlayers.ToList();
+                var random = new Random();
+                int randomIndex = random.Next(playersList.Count);
+                JuggernautPlayer = playersList[randomIndex];
+                SetJuggernautAttributes(JuggernautPlayer);
+                BroadcastMessage($"{killer.Name} killed {victim.Name}! {JuggernautPlayer.Name} is the new Juggernaut!");
+            }
+            else
+            {
+                killer.NumberOfKills++;
+                killer.SetHP(killer.Health + 10);
+                killer.SetGiveDamageMultiplier(killer.Damage + 0.1f);
+                killer.SetRunningSpeedMultiplier(killer.MoveSpeed + 0.1f);
+            }
+
+            // Announce kill streaks
+            if (killer.NumberOfKills % 3 == 0)
+            {
+                BroadcastMessage($"{killer.Name} is on a killing spree!");
             }
         }
-
-        if (!anyZombiePlayer)
+        // check to make sure the juggernaut didn't kill themselves and set to new juggernaut
+        else if (!killer.Team.Equals(JuggernautPlayer) && victim.Team.Equals(JuggernautPlayer))
         {
-            player.IsZombie = true;
-            player.Message("You are the zombie.");
-            player.Kill();
+            JuggernautPlayer = killer;
+            SetJuggernautAttributes(JuggernautPlayer);
+
+
+            // Announce juggernaut transfer
+            BroadcastMessage($"{killer.Name} has become the new Juggernaut!");
         }
-    }
-
-    public override async Task OnAPlayerKilledAnotherPlayer(OnPlayerKillArguments<MyPlayer> args)
-    {
-        if (args.Victim.IsZombie)
-        {
-            args.Victim.IsZombie = false;
-            args.Victim.Message("You are no longer zombie");
-
-            AnnounceShort("Choosing new zombie in 5");
-            await Task.Delay(1000);
-            AnnounceShort("Choosing new zombie in 4");
-            await Task.Delay(1000);
-            AnnounceShort("Choosing new zombie in 3");
-            await Task.Delay(1000);
-            AnnounceShort("Choosing new zombie in 2");
-            await Task.Delay(1000);
-            AnnounceShort("Choosing new zombie in 1");
-            await Task.Delay(1000);
-
-            args.Killer.IsZombie = true;
-            args.Killer.SetHeavyGadget(Gadgets.SledgeHammer.ToString(), 0, true);
-
-            var position = args.Killer.GetPosition();
-        }
-    }
-
-
-    public override async Task<OnPlayerSpawnArguments> OnPlayerSpawning(MyPlayer player, OnPlayerSpawnArguments request)
-    {
-        if (player.IsZombie)
-        {
-            request.Loadout.PrimaryWeapon = default;
-            request.Loadout.SecondaryWeapon = default;
-            request.Loadout.LightGadget = null;
-            request.Loadout.HeavyGadget = Gadgets.SledgeHammer;
-            request.Loadout.Throwable = null;
-        }
-
-        return request;
-    }
-    public override async Task OnPlayerSpawned(MyPlayer player)
-    {
-        if(player.IsZombie)
-        {
-            player.SetRunningSpeedMultiplier(2f);
-            player.SetJumpMultiplier(2f);
-            player.SetFallDamageMultiplier(0f);
-            player.SetReceiveDamageMultiplier(0.1f);
-            player.SetGiveDamageMultiplier(4f);
-        }
-    }
-
-
-
-    public override async Task OnConnected()
-    {
-        await Console.Out.WriteLineAsync("Current state: " + RoundSettings.State);
-
-    }
-    public override async Task OnGameStateChanged(GameState oldState, GameState newState)
-    {
-        await Console.Out.WriteLineAsync("State changed to -> " + newState);
     }
 }
